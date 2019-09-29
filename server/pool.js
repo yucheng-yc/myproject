83a4a92d65624289c00e4d6cdf70501dea37f3a4
yc <318771301@qq.com>
2019-09-29 21:08:41 +0800

    2019/9/29-21:08

--- server/pool.js
+++
@@ -1,9 +0,0 @@
-const mysql=require("mysql");
-const pool=mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"user",
-    connectionLimit:20
-});
-module.exports=pool;
