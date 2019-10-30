0baf7178df4f4dad27b12d3a0a34f5618eea400e
yc <318771301@qq.com>
2019-10-30 18:05:12 +0800

    删除不必要的代码

--- js/index1.js
+++ js/index1.js
@@ -1,10 +1,7 @@
 // 函数入口
 +function(window){
-    // 全局控制对象
-
     // 创建构造函数
     function Yc(){
-
         // 初始化导航栏
             // 控制开关开闭
         this.init.navToggleConfig();
@@ -17,8 +14,6 @@
         
     }
     // 创建vue对象 实现初始化工作
-    
-
     // 全局初始化
     Yc.prototype.init={
         navToggleBtn(){
@@ -40,65 +35,38 @@
         // 先获取当前页面尺寸 然后再将背景bannerImg设置为页面当前尺寸
         ResizemainPageBanner(){
             $('.bannerImg').width($(document).innerWidth());
-        },
-
-        // 当页面改变  页面宽度跟着改变
-        scrollCgSvrImgHgt(){
-            var innerWidth=$(document).innerWidth();
-
-            // 动态修改有bug 不想改了
-
-            // 让ate1和ate2为长方形
-            // 因为有响应式 当屏幕尺寸到725px以下时 会改布局 加判断
-            if(innerWidth>=725){
-                 // 让图片一直是宽高是等比例
-                 $('.blockArticle>article').innerHeight(innerWidth/3);
-
-                $('.ate1,.ate4').innerHeight((innerWidth/3)*2);
-                $('.ate1,.ate4').innerWidth(innerWidth/3);
-            }else{
-                // $('.ate1,.ate4').innerWidth(innerWidth/2);
-                $('.blockArticle>article').innerHeight(innerWidth/4);
-                $('.blockArticle>article').innerWidth(innerWidth/2);
-            }
-            
-
-            
-
-        },
-
+        }
     }
-
     // 轮播图初始化
     Yc.prototype.swiperInit={
         // 首页轮播图
-        // mySwiper:new Swiper('.swiper-container',{
-        //     direction : 'horizontal',
-        //     loop: true, 
+        mySwiper:new Swiper('.swiper-container',{
+            direction : 'horizontal',
+            loop: true, 
             
-        //     // 如果需要分页器
-        //     pagination: {
-        //         clickable: true,
-        //         el: '.swiper-pagination',
-        //         dynamicBullets: true,
-        //     },
+            // 如果需要分页器
+            pagination: {
+                clickable: true,
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+            },
             
-        //     // 如果需要前进后退按钮
-        //     navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        //     },
+            // 如果需要前进后退按钮
+            navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            },
             
-        //     // 如果需要滚动条
-        //     scrollbar: {
-        //     el: '.swiper-scrollbar',
-        //     },
-        //     speed:300,
-        //     autoplay: {
-        //         delay:3000
-        //     },
-        //     effect : 'fade',
-        // }),
+            // 如果需要滚动条
+            scrollbar: {
+            el: '.swiper-scrollbar',
+            },
+            speed:300,
+            autoplay: {
+                delay:3000
+            },
+            effect : 'fade',
+        }),
         // 图文轮播部分
         artImgSwiper:new Swiper('#content',{
             direction : 'horizontal',
@@ -116,10 +84,10 @@
             }
         }),
         // 每个子页面之间的滑动
-        pageSwiper:new Swiper('#pageSwiper',{
-            direction : 'horizontal',
-            autoHeight: true,       
-        })
+        // pageSwiper:new Swiper('#pageSwiper',{
+        //     direction : 'horizontal',
+        //     autoHeight: true,       
+        // })
     }
     // 将构造函数抛到全局
     window.Yc=Yc;
